b26f01b75bb50e0e6efc812987f404baa3b182fb
Lucy Wall <lhwall@gmail.com>
2018-06-12 00:18:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ get "/square/:number" do
 end
 
 get "/say/:number/:phrase" do
-  "#{params[:number].times {puts params[:phrase]}}"
+  "#{params[:number].to_i.times {puts params[:phrase]}}"
 end
 
 
